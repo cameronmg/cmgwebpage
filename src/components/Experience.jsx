7d6bb2d576e4cfc0b908ab5d50637af00a27ad38
cameronmg <camgonza2002@gmail.com>
2022-11-16 15:02:01 -0800

    Bug Fixes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,16 +15,16 @@ const Analytics = () => {
           {/*if the screen is above medium(md) make the text 4xl// if the screen size is above small(sm) make the text 3xl// and on phones make the text 2xl*/}
           {/*define padding of 2rems (py-2)*/}
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-8 text-center text-[#16e4f3] ">Experience</h1>
-          <h2 className=" py-2 text-[#16e4f3]"> Brainlox | Python Instructor | September 2022 - Present</h2>
+          <h2 className=" py-2 text-1xl font-bold text-[#16e4f3]"> Brainlox | Python Instructor | September 2022 - Present</h2>
           <p className="py-1 text-[#16e4f3]"> • Teach kids from ages 7-14 the fundamentals of Python</p>
 
-          <h2 className=" py-2 text-[#16e4f3]"> NASA Lunabotics | LSC - Robotics Software Engineer | January 2022 - Present </h2>
+          <h2 className=" py-2 text-1xl font-bold text-[#16e4f3]"> NASA Lunabotics | LSC - Robotics Software Engineer | January 2022 - Present </h2>
           <p className="py-1 text-[#16e4f3]"> • Helped implement autonomy utilizing the ROS framework </p>
           <p className="py-1 text-[#16e4f3]"> • Contributed to the creation of a network to transfer data between components using NetworkTables </p>
           <p className="py-1 text-[#16e4f3]"> • Implemented a co-processor (Raspberry Pi) that received and forwarded Driver Station input to a Robo-Rio for motor functions</p>
           <p className="py-1 text-[#16e4f3]"> • Employed Java to construct a Network listener for the Robo-Rio to power motors when new values are published on a network</p>
 
-          <h2 className=" py-2 text-[#16e4f3]"> Rice University MolSSI Workshop | Student | June 2022</h2>
+          <h2 className=" py-2 text-1xl font-bold text-[#16e4f3]"> Rice University MolSSI Workshop | Student | June 2022</h2>
           <p className="py-1 text-[#16e4f3]"> • Leveraged Python to develop an API for parsing through molecular data</p>
           <p className="py-1 text-[#16e4f3]"> •  Utilized the Python libraries Matplotlib, Psi4, and NGLview to organize parsed molecular data</p>
           <p className="py-1 text-[#16e4f3]"> •  Collaborated with a group of fellow students to create a program that calculates the molecular energy of an atom</p>
